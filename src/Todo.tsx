917a77e9b7cd4b6de4c200800ee42a6c543b6937
Nyeok <dn.shin98@gmail.com>
2024-11-29 19:59:46 +0900

    [Fix] unnecessary div and styles

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -3,19 +3,20 @@ import Headliner from "./components/Headliner";
 import Header from "./components/Header";
 import ListInput from "./components/ListInput";
 import List from "./components/List";
-/* Styles */
-import styles from "./Todo.module.css";
 
 export const Todo = () => {
   return (
     <>
       <Headliner />
-      <div className={styles.root}>
-        <div className={styles.container}>
-          <Header />
-          <ListInput />
-          <List />
-        </div>
+      <div
+        style={{
+          width: "20vw",
+          height: "100%",
+        }}
+      >
+        <Header />
+        <ListInput />
+        <List />
       </div>
     </>
   );
