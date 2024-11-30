a46be00242a674115914463b71c539618d82bafb
Nyeok <dn.shin98@gmail.com>
2024-11-30 15:03:23 +0900

    [Fix]  component more readable

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -2,29 +2,35 @@
 import Headliner from "./components/Headliner";
 import Header from "./components/Header";
 import ListInput from "./components/ListInput";
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, ReactNode } from "react";
 import Spinner from "./components/Spinner";
 
 const List = lazy(() => import("./components/List"));
+const FallbackSpinner = <Spinner width="100%" height="50px" color="#a2a2a2" />;
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div
+      style={{
+        width: "20vw",
+        height: "100%",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
 const Todo = () => {
   return (
     <>
       <Headliner />
-      <div
-        style={{
-          width: "20vw",
-          height: "100%",
-        }}
-      >
+      <Layout>
         <Header />
         <ListInput />
-        <Suspense
-          fallback={<Spinner width="100%" height="50px" color="#a2a2a2" />}
-        >
+        <Suspense fallback={FallbackSpinner}>
           <List />
         </Suspense>
-      </div>
+      </Layout>
     </>
   );
 };
