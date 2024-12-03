a0fe37d48068b2d737910b417fa90f56b1a38470
Nyeok <dn.shin98@gmail.com>
2024-12-03 17:15:47 +0900

    [Add] Eslint and text compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import compression from "vite-plugin-compression2";
 
 export default defineConfig({
@@ -7,8 +8,10 @@ export default defineConfig({
     rollupOptions: {
       input: "./index.html", // Specify your main HTML file
     },
+    minify: "esbuild", // Minify the JavaScript during build
   },
   plugins: [
+    react(),
     compression({
       algorithm: "gzip",
     }),
